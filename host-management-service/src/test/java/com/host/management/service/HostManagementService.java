c492ff2a64415f7d6dcd9f755ed29e2f17b7801c
Denuwan Himanga Hettiarachi <denuwanonline@gmail.com>
2022-02-19 18:25:40 +0530

    Implement isAvailable method

--- host-management-service/src/test/java/com/host/management/service/HostManagementService.java
+++ host-management-service/src/test/java/com/host/management/service/HostManagementService.java
@@ -5,13 +5,26 @@ import com.host.management.util.Util;
 import org.springframework.stereotype.Service;
 
 import java.util.*;
+import java.util.stream.Collectors;
 
 @Service
 public class HostManagementService {
 
-    public Boolean isHostAvailable(int hostID, Date checkingDate){
+    public Boolean isHostAvailable(int hostID, Date bookingDate) {
         return Util.getHostLibrary().stream()
-                .filter(hst -> hst.getHostID() == hostID && hst.getAvailability().get(checkingDate).equals(true))
+                .filter(hst -> hst.getHostID() == hostID && hst.getAvailability().get(bookingDate).equals(true))
                 .count() > 1l;
+    }
+
+    public Host placeBooking(int hostID, Date bookingDate) {
+        Optional<Host> host = Util.getHostLibrary().stream()
+                .filter(hst -> hst.getHostID() == hostID)
+                .findFirst();
+
+        if(host.isPresent()){
+            host.get().getAvailability().put(bookingDate, false);
+        }
+
+        return host.get();
     }
 }
